3b940ff73b35ea8a2c07f4fa6abf5787ed50f981
Elena Ajayi <131740381+elenaajayi@users.noreply.github.com>
2025-06-29 12:56:34 -0400

    Fix useFrameworkReady import path

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,7 +9,7 @@ import {
   Inter_700Bold
 } from '@expo-google-fonts/inter';
 import * as SplashScreen from 'expo-splash-screen'
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import { useFrameworkReady } from '../hooks/useFrameworkReady';
 
 SplashScreen.preventAutoHideAsync();
 
